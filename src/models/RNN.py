77474b5f950857e550c562ec5f57025c6ab566fc
dodohow1011 <dodohow1011@gmail.com>
2020-06-15 16:21:00 +0000

    first

---
+++ src/models/RNN.py
@@ -0,0 +1,63 @@
+import warnings
+
+import torch
+from torch import nn as nn
+
+
+class BidirectionalGRU(nn.Module):
+
+    def __init__(self, n_in, n_hidden, dropout=0, num_layers=1):
+        super(BidirectionalGRU, self).__init__()
+
+        self.rnn = nn.GRU(n_in, n_hidden, bidirectional=True, dropout=dropout, batch_first=True, num_layers=num_layers)
+
+    def forward(self, input_feat):
+        recurrent, _ = self.rnn(input_feat)
+        return recurrent
+
+class MS(nn.Module):
+    def __init__(self):
+        super(Postnet, self).__init__()
+        self.rnn = BidirectionalGRU(160, 128, dropout=0.5, num_layers=2)
+        self.dense = nn.Linear(256, 10)
+        self.dense_softmax = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = self.rnn(x)
+        strong = self.dense(x)
+        strong = nn.Sigmoid()(strong)
+
+        sof = self.dense_softmax(x)
+        sof = nn.Softmax(dim=1)(sof)
+        sof = torch.clamp(sof, min=1e-7, max=1)
+        weak = (strong * sof).sum(1) / sof.sum(1)
+
+        return strong, weak
+
+class BidirectionalLSTM(nn.Module):
+
+    def __init__(self, nIn, nHidden, nOut, dropout=0, num_layers=1):
+        super(BidirectionalLSTM, self).__init__()
+        self.rnn = nn.LSTM(nIn, nHidden // 2, bidirectional=True, batch_first=True,
+                           dropout=dropout, num_layers=num_layers)
+        self.embedding = nn.Linear(nHidden * 2, nOut)
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+    def load(self, filename=None, parameters=None):
+        if filename is not None:
+            self.load_state_dict(torch.load(filename))
+        elif parameters is not None:
+            self.load_state_dict(parameters)
+        else:
+            raise NotImplementedError("load is a filename or a list of parameters (state_dict)")
+
+    def forward(self, input_feat):
+        recurrent, _ = self.rnn(input_feat)
+        b, T, h = recurrent.size()
+        t_rec = recurrent.contiguous().view(b * T, h)
+
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(b, T, -1)
+        return output
